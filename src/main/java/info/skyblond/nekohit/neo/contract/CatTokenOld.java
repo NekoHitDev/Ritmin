b9a0b1f5b1b5b87d3cb422f706cf79e159c34441
天空Blond <hurui200320@skyblond.info>
2021-05-24 10:40:29 +0800

    Add @SupportedStandars to CatTokenNew.
    Fix the parameter of calling onNEP17Payment in transfer. The invoke parameter should be object[]{from, amount, data},
    instead of data itself, according to https://github.com/neo-project/examples/blob/master/csharp/NEP17/NEP17.Methods.cs#L28
    
    Change the CatTokenNew owner addr. A default account has been included as "ContractOwner" in neo-express config file,
    so we can have a consistent debug experience without changing owner addr back and forth. If should be recognized once
    you start up the devnet.neo-express
    
    Remove signer check in deploy function, since neo express using callByEntity when deploying and CatToken cannot get owner's signature.
    
    Add back issue token and destory token function, so we can change total supply when needed.
    
    Move CatToken to CatTokenOld, now CatTokenNew is the true CatToken.
    
    Comment a lot of code in WcaContract in order to make compiler happy. WcaContract is heavily broken.
    
    Now CatToken can finally be deployed on local network.
    
    Signed-off-by: 天空Blond <hurui200320@skyblond.info>

--- src/main/java/info/skyblond/nekohit/neo/contract/CatTokenOld.java
+++ src/main/java/info/skyblond/nekohit/neo/contract/CatTokenOld.java
@@ -1,44 +1,129 @@
 package info.skyblond.nekohit.neo.contract;
 
+import io.neow3j.devpack.Runtime;
 import info.skyblond.nekohit.neo.domain.WCABasicInfo;
 import info.skyblond.nekohit.neo.domain.WCABuyerInfo;
 import info.skyblond.nekohit.neo.domain.WCAPojo;
-import io.neow3j.devpack.ByteString;
-import io.neow3j.devpack.CallFlags;
-import io.neow3j.devpack.Contract;
-import io.neow3j.devpack.Hash160;
-import io.neow3j.devpack.Runtime;
-import io.neow3j.devpack.Storage;
-import io.neow3j.devpack.StorageContext;
-import io.neow3j.devpack.StorageMap;
-import io.neow3j.devpack.annotations.DisplayName;
-import io.neow3j.devpack.annotations.ManifestExtra;
+import io.neow3j.devpack.*;
+import io.neow3j.devpack.annotations.*;
+import io.neow3j.devpack.contracts.ContractManagement;
 import io.neow3j.devpack.contracts.StdLib;
 import io.neow3j.devpack.events.Event3Args;
 import io.neow3j.devpack.events.Event4Args;
 import io.neow3j.devpack.events.Event5Args;
 
-@ManifestExtra(key = "name", value = "WCA Contract")
-@ManifestExtra(key = "github", value = "https://github.com/NekoHitDev/Ritmin")
-@ManifestExtra(key = "author", value = "Something")
-public class WCAContract {
+@ManifestExtra(key = "name", value = "WCA and CAT Token Contract")
+@ManifestExtra(key = "github", value = "https://github.com/NekoHitDev/GwenDarcy")
+@SupportedStandards("NEP-17")
+public class CatTokenOld {
 
-    private static final StorageContext CTX = Storage.getStorageContext();
+    private static final Hash160 owner = StringLiteralHelper.addressToScriptHash("NVCqzVkjApBWtgKa7c7gbURrJ4dmFYLekS");
 
-    private static final Hash160 CAT_TOKEN_HASH = null; //TODO: set to cat token hash
+    private static final StorageContext storageContext = Storage.getStorageContext();
 
+    @OnVerification
+    public static boolean verify() throws Exception {
+        checkPrivilege();
+        return true;
+    }
+
+    @OnDeployment
+    public static void deploy(Object data, boolean update) throws Exception {
+        checkPrivilege();
+        if (!update) {
+            if (totalSupply() > 0) {
+                throw new Exception("Contract was already deployed.");
+            }
+            // Initialize token supply
+            Storage.put(storageContext, tokenTotalSupplyKey, initialSupply);
+            // And allocate all tokens to the contract owner.
+            tokenAssetMap.put(owner.asByteString(), initialSupply);
+        }
+    }
+
+    // CAT token with NEP-17 standard
+    @DisplayName("Transfer")
+    private static Event3Args<Hash160, Hash160, Integer> onTransfer;
+    private static final String tokenAssetPrefix = "CAT";
+    private static final int initialSupply = 10_000_000_00;
+    private static final String tokenTotalSupplyKey = "tokenTotalSupply";
+    private static final StorageMap tokenAssetMap = storageContext.createMap(tokenAssetPrefix);
+
+    // NEP-17 functions
+    public static int totalSupply() {
+        return Storage.get(storageContext, tokenTotalSupplyKey).toInteger();
+    }
+
+    public static String symbol() {
+        return "CAT";
+    }
+
+    public static int decimals() {
+        return 2;
+    }
+
+    public static int balanceOf(Hash160 account) throws Exception {
+        if (!account.isValid()) {
+            throw new Exception("Argument is not a valid address.");
+        }
+        return getTokenBalance(account);
+    }
+
+    public static boolean transfer(Hash160 from, Hash160 to, int amount, Object[] data) throws Exception {
+        if (!from.isValid() || !to.isValid()) {
+            throw new Exception("From or To address is not a valid address.");
+        }
+        if (amount < 0) {
+            throw new Exception("The transfer amount was negative.");
+        }
+        if (!Runtime.checkWitness(from) && from != Runtime.getCallingScriptHash()) {
+            throw new Exception(
+                    "Invalid sender signature. The sender of the tokens needs to be " + "the signing account.");
+        }
+        if (getTokenBalance(from) < amount) {
+            return false;
+        }
+        if (from != to && amount != 0) {
+            deductTokenFromBalance(from, amount);
+            addTokenToBalance(to, amount);
+        }
+
+        onTransfer.fire(from, to, amount);
+        if (ContractManagement.getContract(to) != null) {
+            Contract.call(to, "onNEP17Payment", CallFlags.ALL, data);
+        }
+
+        return true;
+    }
+
+    // token helper functions
+    private static void addTokenToBalance(Hash160 key, int value) {
+        tokenAssetMap.put(key.asByteString(), getTokenBalance(key) + value);
+    }
+
+    private static void deductTokenFromBalance(Hash160 key, int value) {
+        int oldValue = getTokenBalance(key);
+        if (oldValue == value) {
+            tokenAssetMap.delete(key.asByteString());
+        } else {
+            tokenAssetMap.put(key.asByteString(), oldValue - value);
+        }
+    }
+
+    private static int getTokenBalance(Hash160 key) {
+        return tokenAssetMap.get(key.asByteString()).toInteger();
+    }
+
+    // WCA things, each addr has their own wca info map
     @DisplayName("CreateWCA")
     private static Event5Args<Hash160, Integer, Integer, Integer, String> onCreateWCA;
-
     @DisplayName("BuyWCA")
     private static Event4Args<Hash160, Hash160, String, Integer> onBuyWCA;
-
     @DisplayName("FinishWCA")
     private static Event3Args<Hash160, String, Boolean> onFinishWCA;
 
-    private static final StorageMap wacBasicInfoMap = CTX.createMap("WCA_BASIC_INFO");
-
-    private static final StorageMap wacBuyerInfoMap = CTX.createMap("WCA_BUYER_INFO");
+    private static final StorageMap wacBasicInfoMap = storageContext.createMap("WCA_BASIC_INFO");
+    private static final StorageMap wacBuyerInfoMap = storageContext.createMap("WCA_BUYER_INFO");
 
     private static ByteString getTrueId(Hash160 owner, String identifier) {
         return owner.asByteString().concat(identifier);
@@ -72,17 +157,14 @@ public class WCAContract {
             return "";
         }
 
-        WCAPojo result = new WCAPojo(
-                basicInfo.stakePer100Token,
-                basicInfo.maxTokenSoldCount,
-                buyerInfo.remainTokenCount,
-                basicInfo.endTimestamp
-        );
+        WCAPojo result = new WCAPojo(basicInfo.stakePer100Token, basicInfo.maxTokenSoldCount,
+                buyerInfo.remainTokenCount, basicInfo.endTimestamp);
 
         return StdLib.jsonSerialize(result);
     }
 
-    public static void createWCA(Hash160 owner, int stakePer100Token, int maxTokenSoldCount, int endTimestamp, String identifier) throws Exception {
+    public static void createWCA(Hash160 owner, int stakePer100Token, int maxTokenSoldCount, int endTimestamp,
+            String identifier) throws Exception {
         if (!owner.isValid()) {
             throw new Exception("Owner address is not a valid address.");
         }
@@ -96,8 +178,8 @@ public class WCAContract {
             throw new Exception("The end timestamp was in the past.");
         }
         if (!Runtime.checkWitness(owner) && owner != Runtime.getCallingScriptHash()) {
-            throw new Exception("Invalid sender signature. The sender of the tokens needs to be "
-                    + "the signing account.");
+            throw new Exception(
+                    "Invalid sender signature. The sender of the tokens needs to be " + "the signing account.");
         }
 
         int totalStake = stakePer100Token * maxTokenSoldCount / 100;
@@ -116,7 +198,7 @@ public class WCAContract {
         ByteString basicData = StdLib.serialize(info);
         ByteString buyerData = StdLib.serialize(new WCABuyerInfo(maxTokenSoldCount));
         // remove token from creator
-        // deductTokenFromBalance(owner, totalStake);
+        deductTokenFromBalance(owner, totalStake);
         // store
         wacBasicInfoMap.put(trueId, basicData);
         wacBuyerInfoMap.put(trueId, buyerData);
@@ -132,8 +214,8 @@ public class WCAContract {
             throw new Exception("The token amount was non-positive.");
         }
         if (!Runtime.checkWitness(buyer) && buyer != Runtime.getCallingScriptHash()) {
-            throw new Exception("Invalid sender signature. The sender of the tokens needs to be "
-                    + "the signing account.");
+            throw new Exception(
+                    "Invalid sender signature. The sender of the tokens needs to be " + "the signing account.");
         }
         if (getTokenBalance(buyer) < amount) {
             throw new Exception("Insufficient account balance.");
@@ -162,15 +244,15 @@ public class WCAContract {
         // update buyer info
         buyerInfo.addBuyer(buyer, amount);
         // remove token from buyer
-        // deductTokenFromBalance(buyer, amount);
+        deductTokenFromBalance(buyer, amount);
 
         // store
         ByteString data = StdLib.serialize(buyerInfo);
         wacBuyerInfoMap.put(trueId, data);
         onBuyWCA.fire(buyer, owner, identifier, amount);
         return amount;
-//        return wacBuyerInfoMap.get(trueId).length();
-//        return data.length() + trueId.length();
+        // return wacBuyerInfoMap.get(trueId).length();
+        // return data.length() + trueId.length();
     }
 
     public static boolean finishWCA(Hash160 owner, String identifier, boolean finished) throws Exception {
@@ -180,8 +262,7 @@ public class WCAContract {
         if (finished) {
             // only creator can update WCA to finished
             if (!Runtime.checkWitness(owner) && owner != Runtime.getCallingScriptHash()) {
-                throw new Exception("Invalid caller signature. The caller needs to be "
-                        + "the owner account.");
+                throw new Exception("Invalid caller signature. The caller needs to be " + "the owner account.");
             }
             // otherwise anyone can request to check if wca is end
         }
@@ -204,7 +285,8 @@ public class WCAContract {
                 throw new Exception("You can't finish a terminated WCA.");
             }
             // all amount goes to creator, return stake to creator
-            // addTokenToBalance(owner, buyerInfo.totalAmount + basicInfo.stakePer100Token * basicInfo.maxTokenSoldCount / 100);
+            addTokenToBalance(owner,
+                    buyerInfo.totalAmount + basicInfo.stakePer100Token * basicInfo.maxTokenSoldCount / 100);
             // remove wca
             wacBasicInfoMap.delete(trueId);
             wacBuyerInfoMap.delete(trueId);
@@ -214,12 +296,12 @@ public class WCAContract {
             // return remain stake to creator
             for (int i = 0; i < buyerInfo.buyer.size(); i++) {
                 int stake = buyerInfo.amount.get(i) * basicInfo.stakePer100Token / 100;
-                // addTokenToBalance(buyerInfo.buyer.get(i), buyerInfo.amount.get(i) + stake);
+                addTokenToBalance(buyerInfo.buyer.get(i), buyerInfo.amount.get(i) + stake);
             }
 
             // return remaining stake
             if (buyerInfo.remainTokenCount > 0) {
-                // addTokenToBalance(owner, buyerInfo.remainTokenCount * basicInfo.stakePer100Token / 100);
+                addTokenToBalance(owner, buyerInfo.remainTokenCount * basicInfo.stakePer100Token / 100);
             }
             // remove wca
             wacBasicInfoMap.delete(trueId);
@@ -233,14 +315,55 @@ public class WCAContract {
         return true;
     }
 
+    // admin tools, owner only
     private static void checkPrivilege() throws Exception {
-        // if (!Runtime.checkWitness(owner)) {
-        //     throw new Exception("No authorization.");
-        // }
+        if (!Runtime.checkWitness(owner)) {
+            throw new Exception("No authorization.");
+        }
     }
 
-    private static int getTokenBalance(Hash160 address) {
-        return (int) Contract.call(CAT_TOKEN_HASH, "balanceOf", CallFlags.ALL, new Object[]{address});
+    public static Hash160 contractOwner() {
+        return owner;
     }
-    
+
+    // for token management
+    public static boolean issueNewToken(int amount) throws Exception {
+        checkPrivilege();
+        if (amount <= 0) {
+            return false;
+        }
+        addTokenToBalance(owner, amount);
+        int currentAmount = Storage.get(storageContext, tokenTotalSupplyKey).toInteger();
+        Storage.put(storageContext, tokenTotalSupplyKey, currentAmount + amount);
+        return true;
+    }
+
+    public static boolean destroyToken(int amount) throws Exception {
+        checkPrivilege();
+        if (amount <= 0) {
+            return false;
+        }
+        if (getTokenBalance(owner) < amount) {
+            return false;
+        }
+        deductTokenFromBalance(owner, amount);
+        int currentAmount = Storage.get(storageContext, tokenTotalSupplyKey).toInteger();
+        Storage.put(storageContext, tokenTotalSupplyKey, currentAmount - amount);
+        return true;
+    }
+
+    // for contract management
+    public static void update(ByteString nefFile, String manifest) throws Exception {
+        checkPrivilege();
+        if (nefFile.length() == 0 && manifest.length() == 0) {
+            throw new Exception("The new contract script and manifest must not be empty.");
+        }
+        ContractManagement.update(nefFile, manifest, null);
+    }
+
+    public static void destroy() throws Exception {
+        checkPrivilege();
+        ContractManagement.destroy();
+    }
+
 }
