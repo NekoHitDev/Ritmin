96500a6f938186fe9ffa39f363ff179d9fa3969a
天空Blond <hurui200320@skyblond.info>
2021-06-15 21:35:33 +0800

    upgrade to neow3j 3.11.2, finished the example of refund. test ok
    Signed-off-by: 天空Blond <hurui200320@skyblond.info>

--- src/main/java/info/skyblond/nekohit/neo/domain/WCAPojo.java
+++ src/main/java/info/skyblond/nekohit/neo/domain/WCAPojo.java
@@ -18,7 +18,7 @@ public class WCAPojo {
     public WCAPojo(WCABasicInfo basicInfo, WCABuyerInfo buyerInfo) {
         // This is a workaround since Hash160 convert to int is too big for
         // StdLib.jsonSerialize, so encoded by Base64 first
-        this.ownerBase64 = StdLib.base64Encode(basicInfo.owner.asByteString());
+        this.ownerBase64 = StdLib.base64Encode(basicInfo.owner.toByteString());
         this.stakePer100Token = basicInfo.stakePer100Token;
         this.maxTokenSoldCount = basicInfo.maxTokenSoldCount;
         this.remainTokenCount = buyerInfo.remainTokenCount;
