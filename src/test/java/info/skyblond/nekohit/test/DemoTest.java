2c95a702fd7478bfdae1d9a827d2c9ab2da98f7d
天空Blond <hurui200320@skyblond.info>
2021-06-21 22:17:46 +0800

    Add JUnit 5, add gradle pulgin `test-logger` to beautify test output. Let GitHub Action do the unit test
    Signed-off-by: 天空Blond <hurui200320@skyblond.info>

---
+++ src/test/java/info/skyblond/nekohit/test/DemoTest.java
@@ -0,0 +1,12 @@
+package info.skyblond.nekohit.test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+    @Test
+    void demo(){
+        assertTrue(false, "This must failed!");
+    }
+}
