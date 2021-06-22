aa9ef6b6a578d969219534543d499c80194b9e81
天空Blond <hurui200320@skyblond.info>
2021-06-22 22:16:11 +0800

    Publish Junit test report
    Signed-off-by: 天空Blond <hurui200320@skyblond.info>

--- src/test/java/info/skyblond/nekohit/test/DemoTest.java
+++ src/test/java/info/skyblond/nekohit/test/DemoTest.java
@@ -2,6 +2,7 @@ package info.skyblond.nekohit.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static info.skyblond.nekohit.example.Constants.GAS_TOKEN;
+import static info.skyblond.nekohit.example.Constants.CAT_TOKEN;
 import static info.skyblond.nekohit.example.Constants.CONTRACT_OWNER_ACCOUNT;
 
 import java.io.IOException;
@@ -17,7 +18,12 @@ public class DemoTest {
     public static final Neow3j NEOW3J = Neow3j.build(new HttpService("http://127.0.0.1:50012"));
 
     @Test
-    void demo() throws UnexpectedReturnTypeException, IOException {
+    void passDemo() throws UnexpectedReturnTypeException, IOException {
         assertEquals(0, GAS_TOKEN.getBalanceOf(CONTRACT_OWNER_ACCOUNT).intValue());
+    }
+
+    @Test
+    void failedDemo() throws UnexpectedReturnTypeException, IOException {
+        assertEquals(10, CAT_TOKEN.getBalanceOf(CONTRACT_OWNER_ACCOUNT).intValue());
     }
 }
